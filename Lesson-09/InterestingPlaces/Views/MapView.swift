06a4175e6025a455393367f4eb06688c38a36058
Dominic Swaine <dominic.swaine@gmail.com>
2021-07-10 20:29:10 +0100

    Complete lesson 9 (Filter and Search Maps)

--- Lesson-09/InterestingPlaces/Views/MapView.swift
+++ Lesson-09/InterestingPlaces/Views/MapView.swift
@@ -1,31 +1,3 @@
-/// Copyright (c) 2020 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import SwiftUI
 import MapKit
 
@@ -36,6 +8,7 @@ struct MapView: View {
 
   @State private var region: MKCoordinateRegion
   @State private var mapType: MKMapType = .standard
+  @State private var showSearch = false
   @Environment(\.presentationMode) private var presentationMode
 
   init(location: Place, places: [Place]) {
@@ -55,6 +28,10 @@ struct MapView: View {
             Image(systemName: "xmark.circle.fill")
               .imageScale(.large)
           }
+          Button(action: {showSearch.toggle()}) {
+            Image(systemName: "magnifyingglass.circle.fill")
+              .imageScale(.large)
+          }
           Spacer()
         }
         .padding()
@@ -70,6 +47,9 @@ struct MapView: View {
     }
     .edgesIgnoringSafeArea(.all)
     .navigationBarHidden(true)
+    .sheet(isPresented: $showSearch) {
+      MapSearchView(region: region)
+    }
   }
 }
 
